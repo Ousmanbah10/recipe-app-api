2ca437eec8d91be291025a7bfb5c48c1a46b2f74
Ousmanbah10 <ousmanbah3903244@gmail.com>
2024-06-28 00:43:18 -0500

    Configured Django.

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+CALCULATOR FUNCTIONS
+"""
+
+def add(x,y):
+    """ Add x and y """
+    return x + y
+
+def subtract(x,y):
+    """SUBTRACT NUMBERS"""
+    return x - y
+
---
+++ app/app/test.py
@@ -0,0 +1,20 @@
+"""Sample test"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class Calctest(SimpleTestCase):
+    "Test the calc method"
+    
+    def test_add_numbers(self):
+        "test adding numbers"
+        res = calc.add(5,6)
+        
+        self.assertEqual(res,11)
+    
+    def test_subtract_numbers(self):
+        """TEST Subtract numbers"""
+        res = calc.subract(10,15)
+        
+        self.assertEqual(res,5)    
