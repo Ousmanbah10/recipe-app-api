bedb67204cdee0a6b06adaa8200403e5aa1b0444
Ousmanbah10 <ousmanbah3903244@gmail.com>
2024-06-28 23:36:33 -0500

    Create right database name devbd instead of devdb.

--- app/app/test.py
+++ app/app/test.py
@@ -12,11 +12,10 @@ class Calctest(SimpleTestCase):
     def test_add_numbers(self):
         """test adding numbers"""
         res = calc.add(5, 6)
-        
         self.assertEqual(res, 11)
     
     def test_subtract_numbers(self):
         """TEST Subtract numbers"""
         res = calc.subtract(10, 15)
-        
         self.assertEqual(res, 5)
+
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -12,8 +12,7 @@ from django.test import SimpleTestCase
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
-    """Test commands."""
-    
+    """Test commands."""    
     def test_wait_for_db_ready(self, patched_check):
         """ Test waiting for database if database ready."""
         patched_check.return_value = True
@@ -32,4 +31,3 @@ class CommandTests(SimpleTestCase):
         
         self.assertEqual(patched_check.call_count, 6)      
         patched_check.assert_called_with(databases=['default'])
-        
